c2a3276dc0430d96c0fbb946ca864436be1bed25
Mainery-foxxie <hilosussy@gmail.com>
2026-04-06 19:44:13 +0700

    Update embeds.py

--- discord_bot/visuals/embeds.py
+++ discord_bot/visuals/embeds.py
@@ -1,14 +1,12 @@
 from discord import Embed
 from datetime import datetime
-
 from typing import Optional
 
 
-def exception_embed(exc: str):
+def exception_embed(exc: str) -> Embed:
     embed = Embed(title="An error occurred:",
                   description=f"```py\n{exc}```",
                   color=16711680)
-
     return embed
 
 
@@ -17,7 +15,6 @@ def custom_embed(title: str, description: str) -> Embed:
                   description=description,
                   color=2469096,
                   timestamp=datetime.now())
-
     return embed
 
 
@@ -25,9 +22,6 @@ def loading_embed(title: str, description: Optional[str] = None) -> Embed:
     embed = Embed(description=description,
                   timestamp=datetime.now(),
                   color=2469096)
-
     embed.set_author(name=title,
-                     icon_url="https://cdn.discordapp.com/emojis/1241417265778262086.gif?"
-                              "size=48&quality=lossless&name=Loading")
-
+                     icon_url="https://cdn.discordapp.com/emojis/1241417265778262086.gif?size=48&quality=lossless&name=Loading")
     return embed
