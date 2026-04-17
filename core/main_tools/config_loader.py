ff13a76588bf1ea305416adcc4da8f6dd42fa342
Mainery-foxxie <hilosussy@gmail.com>
2026-04-17 22:40:06 +0700

    Update config_loader.py

--- core/main_tools/config_loader.py
+++ core/main_tools/config_loader.py
@@ -40,7 +40,9 @@ class ConfigLoader:
         self.keep_serials = auto_sell.get("Keep_Serials", 0)
         self.keep_copy = auto_sell.get("Keep_Copy", 0)
         self.creators_blacklist = auto_sell.get("Creators_Blacklist", [])
-        self.default_price_no_competition = auto_sell.get("Default_Price_No_Competition", 1000)   # <-- KEY FIX
+        self.default_price_no_competition = auto_sell.get("Default_Price_No_Competition", 1000)
+        self.auto_sell_copies = auto_sell.get("Auto_Sell_Copies", False)          # NEW
+        self.max_copies_to_sell = auto_sell.get("Max_Copies_To_Sell", 0)          # NEW
 
         under_cut = auto_sell["Under_Cut"]
         self.under_cut_type = under_cut.get("Type", "percent").strip()
